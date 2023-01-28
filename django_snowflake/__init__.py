441d5427f66b86c87214df10c30fb432de723cf4
vinquer <vinquer@gmail.com>
2023-01-28 17:43:39 +0900

    [config] Remove version compatibility enforcement

--- django_snowflake/__init__.py
+++ django_snowflake/__init__.py
@@ -1,10 +1,5 @@
-__version__ = '3.2a2'
+__version__ = '2.2'
 
-# Check Django compatibility before other imports which may fail if the
-# wrong version of Django is installed.
-from .utils import check_django_compatability
-
-check_django_compatability()
 
 from .functions import register_functions  # noqa
 
